ead160ae6a2d6a523597e7282a4d06d234432f2f
Anadi Pandey <anadikrishnapandey@gmail.com>
2020-08-18 16:54:32 +0530

    Addition of multiplication
    this is being done GIT-002

---
+++ Program.py
@@ -0,0 +1,11 @@
+num1= input('Enter first number')
+num2= input('Enter second number')
+
+def Add(num1,num2):
+    sum=num1+num2
+
+def Multiply(num1,num2):
+    pro=num1*num2
+
+print(sum)
+print(pro)
